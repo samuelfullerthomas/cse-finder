2810b8fa7b5c6f9dc3448dfe1613783b9d1eae36
Tommy Kwan <tkwan196@gmail.com>
2016-05-02 08:44:15 +0100

    Babel = async/await!

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  entry: './src/cse-finder.js',
+  entry: ['babel-polyfill', './src/cse-finder.js'],
   output: {
     path: __dirname + '/chrome_extension',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style!css' }
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   }
 }
